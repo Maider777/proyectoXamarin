04911aa949025e83a5728f40558ad16118d05312
Maider777 <maidertxu13@gmail.com>
2023-03-10 09:31:52 +0100

    primer commit

---
+++ Login/Login/Login.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClicked(object sender, EventArgs e)
+        {
+            //verificar datos login
+            string username = "maider";
+            try
+            {
+                if (!txtUsername.Text.Equals(username) || (txtUsername == null))
+                {
+                    DisplayAlert("Alert", "username not correct", "OK");
+                    Console.WriteLine("Error, username not correct");
+                }
+                else
+                {
+                    //if username OK
+                    string password = "a";
+                    if (!txtPassword.Text.Equals(password) || txtPassword == null)
+                    {
+                        DisplayAlert("Alert", "password not correct", "OK");
+                        Console.WriteLine("Error, password not correct");
+                    }
+                    else
+                    {
+                        //if is OK
+                        //open shop menu page
+                        Navigation.PushModalAsync(new ShopMenuPage());
+                        Console.WriteLine("CORRECT");
+                    }
+                }
+            }
+            catch (Exception e2)
+            {
+                Console.WriteLine(e2.Message);
+            }
+            //abrir tienda en linea
+        }
+    }
+}
---
+++ Login/Login/ShopMenuPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Login.ShopMenuPage;
+
+namespace Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShopMenuPage : ContentPage
+    {
+        public ShopMenuPage()
+        {
+            InitializeComponent();
+            mysqlConnection();
+        }
+
+        public class Producto
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public float Precio { get; set; }
+        }
+
+
+        public void mysqlConnection()
+        {
+            try
+            {
+                //crear conexion de la bbdd
+                string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "productos.db3");
+
+                Console.WriteLine(dbPath);
+                var conn = new SQLiteConnection(dbPath);
+
+                conn.CreateTable<Producto>();
+
+                // Insertar un nuevo registro en la tabla
+                var nuevoProducto = new Producto { Nombre = "Ergometro", Precio = 100 };
+                conn.Insert(nuevoProducto);
+
+                // Leer todos los registros de la tabla
+                List<Producto> productos = conn.Table<Producto>().ToList();
+            }
+            catch (Exception e)
+            {
+                DisplayAlert("Alert", e.Message, "OK");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
